9ea728ff785c729313c8788fab1effc59cba5c71
ryandaniels7 <ryansdaniels7@gmail.com>
2022-05-03 12:04:14 -0500

    version 2

--- TrialApp/Form1.Designer.cs
+++ TrialApp/Form1.Designer.cs
@@ -47,7 +47,7 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(54, 15);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Version 1";
+            this.label1.Text = "Version 2";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // Form1
